d810dd965001f720a9f0aa1c57eac332e4f3e844
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2022-11-03 19:08:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict"
+// "use strict"
 
 // const apples = 47;
 // const grapes = 135;
@@ -414,9 +414,13 @@
 
 
 
-const card = [54, 28, 105, 70, 92, 17, 120];
-let total;
-for (i = 0; i < card.length; i += 1) {
-    // total = i + i;
-    console.log(card[i]);
+const cart = [54, 28, 105, 70, 92, 17, 120];
+let total = 0;
+// console.log(card.length);
+for (let i = 0; i < cart.length; i += 1) {
+    console.log(cart[i]);
+
+    total += cart[i];
 }
+
+console.log('Total: ', total);

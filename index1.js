7ed973a4a9a5d019f497e8dba6e77d88d02f2258
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2022-11-23 16:28:47 +0100

    Update index1.js

--- index1.js
+++ index1.js
@@ -622,3 +622,32 @@
 
 
 
+
+const bookShelf = {
+    // Change code below this line
+    books: ["The last kingdom", "The guardian of dreams"],
+    getBooks() {
+      return "Returning all books";
+    },
+    addBook(bookName) {
+      return `Adding book ${bookName}`;
+    },
+    removeBook(bookName) {
+    let indexBook = 0;
+    indexBook = this.books.indexOf(bookName);
+    // console.log(this.books.indexOf(bookName))
+      slipe(indexBook, 1);
+      console.log(this.books)
+      return `Deleting book ${bookName}`;
+    },
+    updateBook(oldName, newName) {
+  
+      return `Updating book ${oldName} to ${newName}`;
+    }
+    // Change code above this line
+  };
+
+  bookShelf.removeBook("The guardian of dreams");
+//    возвращает строку "Deleting book Red sunset"
+  bookShelf.updateBook("Sands of dune", "Dune");
+//    возвращает строку "Updating book Sands of dune to Dune"

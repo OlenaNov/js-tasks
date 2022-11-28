128e73c8227acc28178cfb28aa236c034d1268fd
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2022-11-28 23:43:21 +0100

    Update index1.js

--- index1.js
+++ index1.js
@@ -794,3 +794,175 @@
 // // atTheOldToad.updatePotionName("Stone skn", "Invulnerability potion")
 // // , в свойстве potions будет массив 
 // // // [{ name: "Speed potion", price: 460 }, { name: "Dragon breath", price: 780 }, { name: "Invulnerability potion", price: 520 } ]
+
+
+
+
+
+
+// const pizzaPalace = {
+//     pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+//     order(pizzaName, onSuccess, onError) {
+//       for (let pizza of this.pizzas) {
+//         // console.log(pizza);
+//             if (pizza === pizzaName) {
+                
+//                 return makePizza(pizzaName);
+//             }
+//       };
+//       return onOrderError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+
+//     },
+//   };
+//   // Change code above this line
+  
+//   // Callback for onSuccess
+//   function makePizza(pizzaName) {
+//     return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+//   }
+  
+//   // Callback for onError
+//   function onOrderError(error) {
+//     return `Error! ${error}`;
+//   }
+  
+//   // Method calls with callbacks
+//   console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+//   console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+//   console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+//   console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+  
+
+
+
+
+// function changeEven(numbers, value) {
+//     // Change code below this lin
+//     let newArray = [];
+//   numbers.forEach(number => {
+//     if (number % 2 === 0) {
+//       newArray.push(number += value);
+//     } else {
+//     newArray.push(number);
+// }
+//   });
+//   console.log(newArray);
+//   return newArray;
+
+//     // Change code above this line
+//   };
+
+
+// changeEven([1, 2, 3, 4, 5], 10);
+// // ` возвращает новый массив `[1, 12, 3, 14, 5]`
+// changeEven([2, 8, 3, 7, 4, 6], 10);
+// // ` возвращает новый массив `[12, 18, 3, 7, 14, 16]`
+// changeEven([17, 24, 68, 31, 42], 100);
+// // ` возвращает новый массив `[17, 124, 168, 31, 142]`
+// changeEven([44, 13, 81, 92, 36, 54], 100);
+// // ` возвращает новый массив `[144, 13, 81, 192, 136, 154]`
+// // Вызов функции со случайными, но валидными аргументами, 
+// // возвращает правильное значение.Результатом вызова функции 
+// // changeEven([22,9,7,23,7,21,17], 7 )) будет
+// //  [29,9,7,23,7,21,17]
+
+
+
+
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+// // let arrayplanetsLengths = [];
+// const planetsLengths = planets.map(planet => planet.length);
+// console.log(planetsLengths);
+
+
+
+
+
+
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ["ipsum", "lorem"],
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ["adipisicing", "irure", "velit"],
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ["ex", "culpa", "nostrud"],
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ["non", "amet", "ipsum"],
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ["lorem", "veniam", "culpa"],
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
+
+// const getUserNames = users => {
+//     console.log(getUserNames);
+//     return users.map(user => user.name);
+//   };
+

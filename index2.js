af3ef5f131a0c3dd03d7ed72c6e674bea7f71aaa
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2022-12-06 00:44:01 +0100

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,1 +1,38 @@
 //                MODULE 5
+
+// class StringBuilder {
+  
+//   constructor (initialValue) {
+//     this.value = initialValue;
+//   }
+
+//   getValue() {
+//     return this.value;
+//   }
+
+//   padEnd(str) {
+//     this.value = this.value + str;
+//   }
+
+//   padStart(str) {
+//     this.value = str + this.value;
+//   }
+
+//   padBoth(str) {
+//     this.value = str + this.value + str;
+//   }
+// }
+
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+
+    

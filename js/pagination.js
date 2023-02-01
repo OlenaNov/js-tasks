8d8af7a3c46c3c6f89cdf737178674b9873bb703
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-02-01 20:41:35 +0100

    module-10

---
+++ js/pagination.js
@@ -0,0 +1,13 @@
+
+// const options = {
+//     headers: {
+//         Autorisation: '4330ebfabc654a6992c2aa792f3173a3',
+//     },
+// };
+
+// const url =
+//     'https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1';
+
+//     fetch(url, options)
+//     .then(r => r.json())
+//     .then(console.log());
